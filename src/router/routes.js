0daa3b5eca9b818464a62b0ac1df823b05de8b11
Dana <dana.zhunussova04@gmail.com>
2024-08-15 00:15:54 +0500

    Change vueRouterMode to hash for GitHub Pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'favs', component: () => import('pages/Favs.vue') }
+      { path: '/favs', component: () => import('pages/Favs.vue') }
 
     ]
   },
