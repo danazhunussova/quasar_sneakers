f8516029b839fcf8814c87fe9b9d39829eaf95ab
Dana <dana.zhunussova04@gmail.com>
2024-08-15 22:02:45 +0500

    changing paths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,7 @@
 
             <router-link :to="'/favs'" style="text-decoration:none">
               <q-item class="flex items-center cursor-pointer q-px-md text-grey-7  q-gutter-xs hover:text-black">
-                <img :src="getBasePath + '/heart.svg'" alt="Heart">
+                <img :src="'/heart.svg'" alt="Heart">
                 <span >Favs</span>
               </q-item>
             </router-link>
