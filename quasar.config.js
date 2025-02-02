b786537a5294bcccbb8688dcbde6ee8c99fdf2fe
Dana Zhunussova <dana.zhunussova04@gmail.com>
2025-02-02 14:04:21 +0500

    h

--- quasar.config.js
+++ quasar.config.js
@@ -52,6 +52,7 @@ module.exports = configure(function (/* ctx */) {
       },
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      distDir: 'dist',
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
